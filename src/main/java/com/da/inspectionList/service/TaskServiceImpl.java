3611fd9588cc259558e0f9896f6df4a6accff833
David Adams <david.riding.rails@gmail.com>
2019-07-09 22:18:33 -0700

    add overrides for required methods in TaskServiceImpl

--- src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
+++ src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
@@ -1,8 +1,45 @@
 package com.da.inspectionList.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.da.inspectionList.model.Task;
+import com.da.inspectionList.model.Task.ConstructionType;
+import com.da.inspectionList.model.Task.Result;
+import com.da.inspectionList.model.Task.Status;
 
 @Service
 public class TaskServiceImpl implements TaskServiceInterface {
+
+  @Override
+  public List<Task> findAllTasks() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Task> findAllTasksByStatus(Status status) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Task> findAllTasksByResult(Result result) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Task> findAllTasksByInspector(String inspector) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Task> findAllTasksByConstructionType(ConstructionType constructionType) {
+    // TODO Auto-generated method stub
+    return null;
+  }
   
 }
