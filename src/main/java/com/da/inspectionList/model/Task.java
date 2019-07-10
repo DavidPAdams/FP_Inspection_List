f2136c3a21b177569bb5bf14eec8862f6736f79a
David Adams <david.riding.rails@gmail.com>
2019-07-09 21:57:38 -0700

    add Task model

---
+++ src/main/java/com/da/inspectionList/model/Task.java
@@ -0,0 +1,99 @@
+package com.da.inspectionList.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+  
+  enum ConstructionType {
+    FOUNDATION,
+    WELDING,
+    MASONRY,
+    REBAR
+  }
+  
+  enum Status {
+    INSPECTED,
+    NOT_INSPECTED
+  }
+  
+  enum Result {
+    PASS,
+    FAIL
+  }
+  
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name="task_id")
+  private Long id;
+  
+  private ConstructionType constructionType;
+  private String location;
+  private String inspector;
+  private Status status = Status.NOT_INSPECTED;
+  private Result result;
+  
+  public Task() {};
+  
+  public Task(ConstructionType constructionType, String location, String inspector, Status status) {
+    this.constructionType = constructionType;
+    this.location = location;
+    this.inspector = inspector;
+    this.status = status;
+  }
+
+  public ConstructionType getConstructionType() {
+    return constructionType;
+  }
+
+  public void setConstructionType(ConstructionType constructionType) {
+    this.constructionType = constructionType;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public String getInspector() {
+    return inspector;
+  }
+
+  public void setInspector(String inspector) {
+    this.inspector = inspector;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  public Result getResult() {
+    return result;
+  }
+
+  public void setResult(Result result) {
+    this.result = result;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public String toString() {
+    return "Task [id=" + id + ", constructionType=" + constructionType + ", location=" + location + ", inspector=" + inspector + ", status="
+        + status + ", result=" + result + "]";
+  }
+  
+}
