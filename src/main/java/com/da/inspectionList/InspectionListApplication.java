d10894bbe9c2f591e1ddc148d858f697f64019d9
David Adams <david.riding.rails@gmail.com>
2019-07-09 19:44:30 -0700

    initial commit

---
+++ src/main/java/com/da/inspectionList/InspectionListApplication.java
@@ -0,0 +1,13 @@
+package com.da.inspectionList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionListApplication.class, args);
+	}
+
+}
