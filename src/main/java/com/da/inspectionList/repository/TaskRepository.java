0c7a340b1f6d2a0b0db8dc113a4254d88314aad9
David Adams <david.riding.rails@gmail.com>
2019-07-10 10:27:37 -0700

    revise method signatures in TaskRepository

--- src/main/java/com/da/inspectionList/repository/TaskRepository.java
+++ src/main/java/com/da/inspectionList/repository/TaskRepository.java
@@ -12,9 +12,10 @@ import com.da.inspectionList.model.Task.Status;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-  public List<Task> findAllTasks();
+  public List<Task> findAll();
   public List<Task> findAllTasksByStatus(Status status);
   public List<Task> findAllTasksByResult(Result result);
   public List<Task> findAllTasksByInspector(String inspector);
   public List<Task> findAllTasksByConstructionType(ConstructionType constructionType);
+  public Task findTaskById(Long task_id);
 }
