bbc21b6206f11456247e259c2f394cacf4d00a89
David Adams <david.riding.rails@gmail.com>
2019-07-10 10:28:40 -0700

    revise method signatures in TaskServiceInterface

--- src/main/java/com/da/inspectionList/service/TaskServiceInterface.java
+++ src/main/java/com/da/inspectionList/service/TaskServiceInterface.java
@@ -8,9 +8,10 @@ import com.da.inspectionList.model.Task.Result;
 import com.da.inspectionList.model.Task.Status;
 
 public interface TaskServiceInterface {
-  public List<Task> findAllTasks();
+  public List<Task> findAll();
   public List<Task> findAllTasksByStatus(Status status);
   public List<Task> findAllTasksByResult(Result result);
   public List<Task> findAllTasksByInspector(String inspector);
   public List<Task> findAllTasksByConstructionType(ConstructionType constructionType);
+  public Task findTaskById(Long task_id);
 }
