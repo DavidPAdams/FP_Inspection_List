c8f7f3cfd49d40d40034459402405c89349af5e0
David Adams <david.riding.rails@gmail.com>
2019-07-09 22:17:57 -0700

    add method signatures to TaskServiceInterface

--- src/main/java/com/da/inspectionList/service/TaskServiceInterface.java
+++ src/main/java/com/da/inspectionList/service/TaskServiceInterface.java
@@ -1,5 +1,16 @@
 package com.da.inspectionList.service;
 
-public interface TaskServiceInterface {
+import java.util.List;
 
+import com.da.inspectionList.model.Task;
+import com.da.inspectionList.model.Task.ConstructionType;
+import com.da.inspectionList.model.Task.Result;
+import com.da.inspectionList.model.Task.Status;
+
+public interface TaskServiceInterface {
+  public List<Task> findAllTasks();
+  public List<Task> findAllTasksByStatus(Status status);
+  public List<Task> findAllTasksByResult(Result result);
+  public List<Task> findAllTasksByInspector(String inspector);
+  public List<Task> findAllTasksByConstructionType(ConstructionType constructionType);
 }
