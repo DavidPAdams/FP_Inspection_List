5038fe7af44eb38f05ce53f4e523b181e4621f81
David Adams <david.riding.rails@gmail.com>
2019-07-09 22:17:33 -0700

    add search method signatures to TaskRepository

--- src/main/java/com/da/inspectionList/repository/TaskRepository.java
+++ src/main/java/com/da/inspectionList/repository/TaskRepository.java
@@ -1,11 +1,20 @@
 package com.da.inspectionList.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.da.inspectionList.model.Task;
+import com.da.inspectionList.model.Task.ConstructionType;
+import com.da.inspectionList.model.Task.Result;
+import com.da.inspectionList.model.Task.Status;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-  
+  public List<Task> findAllTasks();
+  public List<Task> findAllTasksByStatus(Status status);
+  public List<Task> findAllTasksByResult(Result result);
+  public List<Task> findAllTasksByInspector(String inspector);
+  public List<Task> findAllTasksByConstructionType(ConstructionType constructionType);
 }
