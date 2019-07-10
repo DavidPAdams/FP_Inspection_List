729101693f3becaaf14d5555fb7ff1bfb993cd9b
David Adams <david.riding.rails@gmail.com>
2019-07-10 10:28:15 -0700

    define methods in TaskServiceImpl

--- src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
+++ src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
@@ -2,44 +2,54 @@ package com.da.inspectionList.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.da.inspectionList.model.Task;
 import com.da.inspectionList.model.Task.ConstructionType;
 import com.da.inspectionList.model.Task.Result;
 import com.da.inspectionList.model.Task.Status;
+import com.da.inspectionList.repository.TaskRepository;
 
 @Service
 public class TaskServiceImpl implements TaskServiceInterface {
+  
+  @Autowired
+  private TaskRepository taskRepository;
 
-  @Override
-  public List<Task> findAllTasks() {
-    // TODO Auto-generated method stub
-    return null;
+  public List<Task> findAll() {
+    List<Task> tasks = taskRepository.findAll();
+    return tasks;
   }
 
   @Override
   public List<Task> findAllTasksByStatus(Status status) {
-    // TODO Auto-generated method stub
-    return null;
+    List<Task> tasks = taskRepository.findAllTasksByStatus(status);
+    return tasks;
   }
 
   @Override
   public List<Task> findAllTasksByResult(Result result) {
-    // TODO Auto-generated method stub
-    return null;
+    List<Task> tasks = taskRepository.findAllTasksByResult(result);
+    return tasks;
   }
 
   @Override
   public List<Task> findAllTasksByInspector(String inspector) {
-    // TODO Auto-generated method stub
-    return null;
+    List<Task> tasks = taskRepository.findAllTasksByInspector(inspector);
+    return tasks;
   }
 
   @Override
   public List<Task> findAllTasksByConstructionType(ConstructionType constructionType) {
-    // TODO Auto-generated method stub
-    return null;
+    List<Task> tasks = taskRepository.findAllTasksByConstructionType(constructionType);
+    return tasks;
+  }
+  
+  @Override
+  public Task findTaskById(Long task_id) {
+    Task task = taskRepository.findTaskById(task_id);
+    return task;
   }
   
 }
