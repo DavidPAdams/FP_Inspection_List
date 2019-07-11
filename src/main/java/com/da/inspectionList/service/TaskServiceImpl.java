b9d1a4d17dc605d4b8cdd713e626da848ed87894
David Adams <david.riding.rails@gmail.com>
2019-07-11 12:18:47 -0700

    revise methods in TaskServiceImpl

--- src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
+++ src/main/java/com/da/inspectionList/service/TaskServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.da.inspectionList.model.Task;
 import com.da.inspectionList.model.Task.ConstructionType;
-import com.da.inspectionList.model.Task.Result;
-import com.da.inspectionList.model.Task.Status;
 import com.da.inspectionList.repository.TaskRepository;
 
 @Service
@@ -17,26 +15,9 @@ public class TaskServiceImpl implements TaskServiceInterface {
   @Autowired
   private TaskRepository taskRepository;
 
-  public List<Task> findAll() {
-    List<Task> tasks = taskRepository.findAll();
-    return tasks;
-  }
-
   @Override
-  public List<Task> findAllTasksByStatus(Status status) {
-    List<Task> tasks = taskRepository.findAllTasksByStatus(status);
-    return tasks;
-  }
-
-  @Override
-  public List<Task> findAllTasksByResult(Result result) {
-    List<Task> tasks = taskRepository.findAllTasksByResult(result);
-    return tasks;
-  }
-
-  @Override
-  public List<Task> findAllTasksByInspector(String inspector) {
-    List<Task> tasks = taskRepository.findAllTasksByInspector(inspector);
+  public List<Task> findAllTasks() {
+    List<Task> tasks = taskRepository.findAllTasks();
     return tasks;
   }
 
