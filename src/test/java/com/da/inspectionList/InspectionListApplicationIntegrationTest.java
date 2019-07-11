05eeff4a659598d0f93e11ee79c7073f05f94bf1
David Adams <david.riding.rails@gmail.com>
2019-07-11 14:07:28 -0700

    rename file ApplicationTests -> ApplicationIntegrationTest

--- src/test/java/com/da/inspectionList/InspectionListApplicationIntegrationTest.java
+++ src/test/java/com/da/inspectionList/InspectionListApplicationIntegrationTest.java
@@ -1,28 +1,20 @@
 package com.da.inspectionList;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.da.inspectionList.model.Task;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class InspectionListApplicationTests {
+public class InspectionListApplicationIntegrationTest {
 
 	@Test
 	@Ignore
 	public void contextLoads() {
 	}
-	
-	@Test
-	public void taskDomainExistsTDD() {
-	  Task testTask = new Task();
-	  assertThat(testTask.getStatus()).isNotEqualTo(null);
-	}
 
 }
+
+
